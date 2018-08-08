8f04ff8c79be46c2a863f21956369a7c0aa97ca3
adeadedoja <damdey@yahoo.com>
2018-08-08 14:17:04 +0200

    able to add new reservation

--- src/app/reservation-create/reservation-create.component.ts
+++ src/app/reservation-create/reservation-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-create',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationCreateComponent implements OnInit {
 
-  constructor() { }
+  reservationForm: FormGroup;
+  name:string = '';
+  departure_city:string = '';
+  arrival_city:string = '';
+  departure_date:string = '';
+  return_date:string = '';
+  class:string = '';
+  seat:string = '';
+
+
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.reservationForm = this.formBuilder.group({
+      'name': [null, Validators.required],
+      'departure_city': [null, Validators.required],
+      'arrival_city': [null, Validators.required],
+      'departure_date': [null, Validators.required],
+      'return_date': [null, Validators.required],
+      'class': [null, Validators.required],
+      'seat': [null, Validators.required]
+    })
+  }
+
+  onFormSubmit(form:NgForm) {
+    this.api.postReservation(form).subscribe( res => {
+        let id = res['_id'];
+        this.router.navigate(['/reservation-details', id]);
+    }, err => {
+      console.log(err);
+    })
   }
 
 }
