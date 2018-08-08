4777312d7d82e830375bcd5a5aa55d9a3030c4e0
adeadedoja <damdey@yahoo.com>
2018-08-08 11:55:14 +0200

    create components for index, detail, create and edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { ReservationDetailComponent } from './reservation-detail/reservation-detail.component';
+import { ReservationCreateComponent } from './reservation-create/reservation-create.component';
+import { ReservationEditComponent } from './reservation-edit/reservation-edit.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'reservations',
+    component: ReservationComponent,
+    data: { title: 'Reservation List' }
+  },
+  {
+    path: 'reservation-details/:id',
+    component: ReservationDetailComponent,
+    data: { title: 'Reservation Details' }
+  },
+  {
+    path: 'reservation-create',
+    component: ReservationCreateComponent,
+    data: { title: 'Reservation Book' }
+  },
+  {
+    path: 'reservation-edit/:id',
+    component: ReservationEditComponent,
+    data: { title: 'Reservation Book' }
+  },
+  {
+    path: '',
+    redirectTo: '/reservations',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservationComponent,
+    ReservationDetailComponent,
+    ReservationCreateComponent,
+    ReservationEditComponent
   ],
   imports: [
     BrowserModule
